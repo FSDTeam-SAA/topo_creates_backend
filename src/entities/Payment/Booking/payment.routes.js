16784033033586e2483b38f0de6a21d59f5258a0
jarifMohammed <mohammedjarif@protonmail.com>
2025-09-04 01:14:52 +0600

    feat: Update checkout session route to include authentication middleware

--- src/entities/Payment/Booking/payment.routes.js
+++ src/entities/Payment/Booking/payment.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { createBookingPaymentService } from "./payment.service.js";
+import { userMiddleware, verifyToken } from "../../../core/middlewares/authMiddleware.js";
 
 
 
 const router = express.Router()
-router.post("/create-checkout-session", protect, createBookingPaymentController);
+router.post("/create-checkout-session", verifyToken,userMiddleware, createBookingPaymentService);
 
 export default router
