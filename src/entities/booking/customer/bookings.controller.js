ecc3f12a67f18a2268493fb1401bb910c778769c
jishad10 <jishadjishad49@gmail.com>
2025-12-06 09:31:25 +0600

    fix: Update user ID reference in createBookingController and clean up createBookingService

--- src/entities/booking/customer/bookings.controller.js
+++ src/entities/booking/customer/bookings.controller.js
@@ -4,7 +4,7 @@ import { createBookingService, deleteBookingService, getAllBookingsService, getB
 
 export const createBookingController = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
     const role = req.user.role; 
 
     const booking = await createBookingService({
