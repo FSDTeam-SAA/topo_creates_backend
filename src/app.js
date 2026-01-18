6acc669ceec6826804544e2154313adc02ee8139
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-18 23:22:56 +0600

    fix: Correct CORS origin configuration to include localhost for development

--- src/app.js
+++ src/app.js
@@ -105,7 +105,7 @@ const server = createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: ["https://musegala.com.au", "https://www.musegala.com.au,","https://api.musegala.com.au"],
+    origin: ["https://musegala.com.au", "https://www.musegala.com.au,","https://api.musegala.com.au","http://localost:3000",
     methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH",'OPTIONS'],
     credentials: true,
   },
